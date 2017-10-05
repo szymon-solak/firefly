c2157cccbb6d3d7d7a15404aa06832bbfc615790
Yoshuro <yoshuro@hotmail.com>
2017-10-05 23:57:13 +0200

    Fix order

--- server/src/util/deepEqual.js
+++ server/src/util/deepEqual.js
@@ -1,11 +1,11 @@
 export const deepEqual = (src, comp) => {
-  if (Object.getOwnPropertyNames(src).length !== Object.getOwnPropertyNames(comp).length) return false
   if (
     src === undefined ||
     src === null ||
     comp === undefined ||
     comp === null
   ) return false
+  if (Object.getOwnPropertyNames(src).length !== Object.getOwnPropertyNames(comp).length) return false
   return Object.keys(src).every((key) => {
     if (typeof src[key] === 'object') {
       return deepEqual(src[key], comp[key])
