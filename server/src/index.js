010798b069282e4bde4fe30695621427ac840a3f
Yoshuro <yoshuro@hotmail.com>
2018-08-19 16:36:55 +0200

    Update weather service behaviour

--- server/src/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app'
-import { log } from './util'
-
-const port = process.env.PORT || 8080
-
-app.listen(port, function() {
-  const host = this.address().address
-  log.info(`Firefly server is listening at http://${host}:${port}`)
-})
--- server/src/services/weather/index.js
+++ server/src/services/weather/index.js
@@ -18,14 +18,20 @@ export const weather = createFetchService({
   name: 'weather',
   attachMethods: [handleWeatherData],
   init() {
-    const link = process.env.NODE_ENV === 'production'
-      ? `${api.link}?id=${api.cityId}&APPID=${api.token}&units=metric`
-      : 'http://127.0.0.1:8080/stub'
+    const isProd = process.env.NODE_ENV === 'production'
 
-    this.fetch(link, handleWeatherData)
-    this.interval(
-      () => this.fetch(link, handleWeatherData),
-      api.interval
-    )
+    /* Do not run this service outside of production
+     * as it has low rate limit
+     */
+
+    if (isProd) {
+      const link = `${api.link}?id=${api.cityId}&APPID=${api.token}&units=metric`
+
+      this.fetch(link, handleWeatherData)
+      this.interval(
+        () => this.fetch(link, handleWeatherData),
+        api.interval
+      )
+    }
   },
 })
