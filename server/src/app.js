83a0233dd238c1e8b303c2044131ff5ef5f8432d
Yoshuro <yoshuro@hotmail.com>
2018-08-19 16:36:12 +0200

    Remove Express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,12 @@
-import express from 'express'
-import Websocket from 'uws'
+import Websocket from 'ws'
 
 import { mainframe } from './mainframe'
 
-const app = express()
-const router = express.Router()
-
-// Websockets
 export const wss = new Websocket.Server({ port: 8090 })
 
 mainframe.init()
 
+// Send all current states to newly connected clients
 wss.on('connection', (ws) => {
   const states = mainframe.getStates()
 
@@ -18,35 +14,3 @@ wss.on('connection', (ws) => {
     ws.send(JSON.stringify(state))
   })
 })
-
-// Express + Router
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-router.get('/stub', (req, res) => {
-  res.send({
-    weather: [
-      {
-        id: 300,
-        main: 'Drizzle',
-        description: 'light intensity drizzle',
-        icon: '09d',
-      },
-    ],
-    main: {
-      temp: 123,
-      pressure: 1012,
-      humidity: 81,
-      temp_min: 217,
-      temp_max: 230,
-    },
-    wind: {
-      speed: 4.1,
-    },
-  })
-})
-
-app.use('/', router)
-
-export default app
